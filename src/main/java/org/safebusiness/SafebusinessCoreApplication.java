8bfbec6996bd695954b0994ec8fdd7f86435bced
samuelmale <samuelsmalek@gmail.com>
2019-07-26 11:59:08 +0300

    Correcting package naming

--- src/main/java/org/safebusiness/SafebusinessCoreApplication.java
+++ src/main/java/org/safebusiness/SafebusinessCoreApplication.java
@@ -1,4 +1,4 @@
-package org.safebusiness.safebusinesscore;
+package org.safebusiness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/org/safebusiness/SafebusinessCoreApplicationTests.java
+++ src/test/java/org/safebusiness/SafebusinessCoreApplicationTests.java
@@ -1,4 +1,4 @@
-package org.safebusiness.safebusinesscore;
+package org.safebusiness;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
