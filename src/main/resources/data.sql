68ba24d020b78bb97ee4db445262a7d5c8b14c44
samuelmale <samuelsmalek@gmail.com>
2019-07-28 21:05:41 +0300

    Trunk-11: Fixing 'H2 DB doesn't support INSERT IGNORE'

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 -- Create Admin User
-INSERT IGNORE INTO user (id, username, password) VALUES (1, 'admin', 'Admin123');
+-- INSERT IGNORE INTO user (id, username, password) VALUES (1, 'admin', 'Admin123');
+-- H2 DB doesn't support `INSERT IGNORE`
+
+-- HACKY WORK AROUND
+REPLACE INTO user (id, username, password) VALUES (1, 'admin', 'Admin123');
