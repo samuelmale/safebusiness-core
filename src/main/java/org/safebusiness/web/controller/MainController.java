f716b431744a50b5e71a459b863a1e5f72084647
samuelmale <samuelsmalek@gmail.com>
2019-07-31 21:37:56 +0300

    Patch 4

--- src/main/java/org/safebusiness/web/controller/MainController.java
+++ src/main/java/org/safebusiness/web/controller/MainController.java
@@ -1,16 +1,19 @@
 package org.safebusiness.web.controller;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.validation.Valid;
 
+import org.apache.commons.collections.IteratorUtils;
 import org.safebusiness.Article;
 import org.safebusiness.api.repo.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -29,16 +32,43 @@ public class MainController {
 		return "redirect:safebusiness/index";
 	}
 	
-	@GetMapping("safebusiness/newArticle")
-	public String newArticle(Model model) {
-		model.addAttribute("article", new Article());
-		return "addArticle";
+	@GetMapping("safebusiness/article/{id}")
+	public String createOrViewArticle(Model model, @PathVariable("id") String id) {
+		if (id != null) {
+			try {
+				if (articleRepo.findById(Integer.parseInt(id)).isPresent()) {
+					model.addAttribute("article", articleRepo.findById(Integer.parseInt(id)).get());
+					model.addAttribute("inViewMode", true);
+				} else {
+					model.addAttribute("inViewMode", false);
+					model.addAttribute("article", new Article());
+				}
+			} catch(NumberFormatException ex) {
+				model.addAttribute("inViewMode", false);
+				model.addAttribute("article", new Article());
+			}
+			
+		} else {
+			model.addAttribute("inViewMode", false);
+			model.addAttribute("article", new Article());
+		}
+		
+		return "article";
 	}
 	
 	@PostMapping("addArticle")
 	public String addArticle(@Valid Article article) {
 		articleRepo.save(article);
 		return "redirect:safebusiness/index";
+	}
+	
+	@GetMapping("safebusiness/listArticles")
+	public String listArticles(Model model) {
+		Iterable<Article> interator = articleRepo.findAll();
+		@SuppressWarnings("unchecked")
+		List<Article> articles = interator != null ? IteratorUtils.toList(interator.iterator()) : new ArrayList<>();
+		model.addAttribute("articles", articles);
+		return "listArticles";
 	}
 	
 	private List<Article> parseArticleString(String val) {
